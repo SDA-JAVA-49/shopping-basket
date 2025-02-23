89269ebe896e01072c9db5755c0903d9dcfe7ff3
armirigj <armirgjini76@gmail.com>
2025-02-23 11:28:54 +0100

    Task 11

--- src/main/java/al/sda/app/Application.java
+++ src/main/java/al/sda/app/Application.java
@@ -1,4 +1,24 @@
 package al.sda.app;
 
+import al.sda.app.basket.model.GenericProduct;
+import al.sda.app.basket.model.Product;
+import al.sda.app.basket.repository.BasketRepository;
+import al.sda.app.basket.service.BasketService;
+
 public class Application {
+    public static void main(String[] args){
+        BasketRepository basketRepository = new BasketRepository();
+
+        Product pasta = new GenericProduct();
+        Product apple = new GenericProduct();
+        Product rice = new GenericProduct();
+        Product milk = new GenericProduct();
+
+        basketRepository.addProduct(pasta);
+        basketRepository.addProduct(apple);
+        basketRepository.addProduct(rice);
+        basketRepository.addProduct(milk);
+
+
+    }
 }
